c0b5e19894945e1a647be0367b88055c84a0cdfb
Ivan Stadnik <stadnikxivan@gmail.com>
2020-10-19 22:07:05 +0200

    init: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const formatedBracketsConfig = bracketsConfig.map(el => el.join(''));
+
+  function deletePairs(bracketsString) {
+    if (!bracketsString.length) return true;
+  
+    const formatedString = formatedBracketsConfig.reduce((acc, rule) => acc.replace(rule, ''), bracketsString);
+  
+    if (formatedString === bracketsString) return false;
+  
+    return deletePairs(formatedString);
+  };
+
+  return deletePairs(str);
+};
